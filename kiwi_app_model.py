9f9ba2c6b5a0464eca8afea79aafe138de5a3116
MuhammadIrsyhad <muhammadirsyhad.22@ichat.sp.edu.sg>
2025-01-14 19:01:09 +0800

    cleanup

---
+++ kiwi_app_model.py
@@ -0,0 +1,7 @@
+from tensorflow.keras.applications.inception_v3 import InceptionV3
+from tensorflow.keras.layers import Flatten, Dense, Dropout
+from tensorflow.keras.models import Model
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+import os
+

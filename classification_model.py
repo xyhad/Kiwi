66b0250c293661a654ed81c705d8f0edf90d6bcd
MuhammadIrsyhad <muhammadirsyhad.22@ichat.sp.edu.sg>
2025-01-21 17:41:36 +0800

    Created new model

--- classification_model.py
+++ classification_model.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, Input
 from tensorflow.keras import layers, models, Input
 from tensorflow.keras.utils import image_dataset_from_directory
 from tensorflow.keras.callbacks import EarlyStopping
@@ -14,14 +16,14 @@ train_dataset = tf.keras.utils.image_dataset_from_directory(
     train_dir,
     seed=123, 
     image_size=(75, 75),
-    batch_size=32
+    batch_size=64
 )
 
 val_dataset = tf.keras.utils.image_dataset_from_directory(
     valid_dir,
     seed=123,
     image_size=(75, 75),
-    batch_size=32
+    batch_size=64
 )
 
 
@@ -34,23 +36,21 @@ train_dataset = train_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 val_dataset = val_dataset.prefetch(buffer_size=tf.data.AUTOTUNE)
 
 
-model = models.Sequential([
-    Input(shape=(75, 75, 3)),
-    layers.Conv2D(32, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.Flatten(),
-    layers.Dense(64, activation='relu'),
-    layers.Dense(3)
-])
+model = Sequential()
 
-model.compile(
-    optimizer='adam',
-    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-    metrics=['accuracy']
-)
+
+model.add(Conv2D(filters=128, kernel_size=3, strides=(1, 1), padding='same', activation='relu', input_shape=(75, 75, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+model.add(Conv2D(filters=256, kernel_size=3, strides=(1, 1), padding='same', activation='relu', input_shape=(75, 75, 3)))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2), padding='valid'))
+model.add(Dropout(0.25))
+model.add(Flatten())
+model.add(Dense(units=128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(units=3, activation='softmax'))
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
 
 early_stopping = EarlyStopping(
     monitor='val_loss',
@@ -62,12 +62,14 @@ history = model.fit(
     train_dataset,
     validation_data=val_dataset,
     epochs=30,
+    steps_per_epoch=30,
     callbacks=[early_stopping]
 )
 
 
 loss, accuracy = model.evaluate(val_dataset)
 print("Validation Accuracy:", accuracy)
+print("Number of epochs completed:", len(history.history['accuracy']))
 
 plt.figure(figsize=(12, 5))
 
@@ -76,16 +78,16 @@ plt.plot(history.history['accuracy'], label='Training Accuracy')
 plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
 plt.xlabel('Epoch')
 plt.ylabel('Accuracy')
-plt.ylim([0.5, 1])
+plt.ylim([0.5, 1]) 
 plt.legend(loc='lower right')
 plt.title('Training and Validation Accuracy')
 
-plt.subplot(1, 2, 2) 
+plt.subplot(1, 2, 2)
 plt.plot(history.history['loss'], label='Training Loss')
 plt.plot(history.history['val_loss'], label='Validation Loss')
 plt.xlabel('Epoch')
 plt.ylabel('Loss')
-plt.ylim([0, max(max(history.history['loss']), max(history.history['val_loss'])) * 1.1])  # Adjust y-axis limit dynamically
+plt.ylim([0, max(max(history.history['loss']), max(history.history['val_loss'])) * 1.1])  # Adjust the range dynamically
 plt.legend(loc='upper right')
 plt.title('Training and Validation Loss')
 
@@ -99,7 +101,7 @@ test_dataset = tf.keras.utils.image_dataset_from_directory(
     test_dir,
     seed=123,
     image_size=(75, 75),
-    batch_size=32
+    batch_size=64
 )
 
 
